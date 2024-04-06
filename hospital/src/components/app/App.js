6126ff480d7f5597aa59140b7d48ef485320fc8b
Maksym-Yurii.Rudko <m.rudko@abtollc.com>
2024-04-06 23:10:58 +0300

    Refactored main page to allow for better reusability

--- hospital/src/components/app/App.js
+++ hospital/src/components/app/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import doctorsPhoto from './images/doctors.jpg';
+import HospitalHeader from './Header';
+import doctorsPhoto from './images/doctors.png';
 import contactIcon from './images/contacts.png';
 import newsIcon from './images/news.png';
 import departmentsIcon from './images/departments.png';
 import healthSchoolIcon from './images/health_school.png';
 
 import {Col, Image, Navbar, NavbarText, Row} from 'react-bootstrap';
+import {VerticalDivider, VerticalSpacer} from "./SpacingElements";
 
 const BigActionButtons = [{text: 'Новини', image_src: newsIcon}, {
     text: 'Контакти',
@@ -16,36 +18,17 @@ const BigActionButtons = [{text: 'Новини', image_src: newsIcon}, {
 function App() {
     return (<div className="App">
         <header className='pt-4'>
-            <HeaderContent/>
+            <HospitalHeader/>
         </header>
 
         <Row className='p-4 me-0 ms-0'>
-            <Col className='col-4'>
-                <div id='rounding-box' className='rounded-5'>
-                    <Image src={doctorsPhoto} alt="Doctors" className='rounded-5' fluid/>
-                </div>
-            </Col>
-            <Col className='col-8 front-page-motto-buttons'>
-                <Row className='h-100'>
-                    <h2 className='text-center primary-text w-75 mx-auto my-auto front-page-motto'>
-                        Обирайте нас для себе та своєї родини тому що ми найкращі
-                    </h2>
-                </Row>
-                <Row>
-                    <div className='d-flex justify-content-center front-page-buttons'>
-                        <button className='my-button primary col-4'>Записатись на прийом</button>
-                        <button className='my-button col-4'>Інструкція запису</button>
-                    </div>
-                </Row>
-            </Col>
-            <div id='some-space-idgaf' style={{height: '4rem'}}></div>
+            <VerticalSpacer height='2rem'/>
+            <MottoMenu/>
+            <VerticalSpacer height='4rem'/>
         </Row>
 
         <Row className='column-gap-0 m-0 p-0 bg-white'>
-            {BigActionButtons.map((button, index, arr) => (<Col className='col-3 p-0 m-0'>
-                <BigActionButton text={button.text} image_src={button.image_src}
-                                 isLast={index === arr.length - 1}/>
-            </Col>))}
+            <BigTileMainMenu/>
         </Row>
 
         {/*<div className="GridContainer">*/}
@@ -58,21 +41,39 @@ function App() {
     </div>);
 }
 
-function HeaderContent() {
+function MottoMenu() {
     return <>
-        <Navbar expand="md" sticky="top" className="primary primary-floating rounded-pill px-3 mx-4 me-4">
-            <Navbar.Brand>
-                <NavbarText className='text-white ms-2'>
-                    КНП "Мостиська міська лікарня"
-                </NavbarText>
-            </Navbar.Brand>
-            <Navbar.Collapse className='justify-content-end '>
-                <div className="RightContent me-3">м.Мостиська, вул. Я.Мудрого, 111</div>
-                <div className="OrangePart rounded-pill px-3">+38 091 234 56 78</div>
-            </Navbar.Collapse>
-        </Navbar>
+        <Col className='col-4'>
+            <div id='rounding-box' className='rounded-5'>
+                <Image src={doctorsPhoto} alt="Doctors" className='rounded-5' fluid/>
+            </div>
+        </Col>
+        <Col className='col-8 front-page-motto-buttons'>
+            <Row className='h-100'>
+                <h2 className='text-center primary-text w-75 mx-auto my-auto front-page-motto'>
+                    Обирайте нас для себе та своєї родини тому що ми найкращі
+                </h2>
+            </Row>
+            <Row>
+                <div className='d-flex justify-content-center front-page-buttons'>
+                    <button className='my-button primary col-4'>Записатись на прийом</button>
+                    <button className='my-button col-4'>Інструкція запису</button>
+                </div>
+            </Row>
+        </Col>
     </>
 }
+
+function BigTileMainMenu() {
+    return <>
+        {BigActionButtons.map((button, index, arr) => (
+            <Col className='col-3 p-0 m-0'>
+                <BigActionButton text={button.text} image_src={button.image_src}
+                                 isLast={index === arr.length - 1}/>
+            </Col>))}
+    </>
+}
+
 
 function BigActionButton(props) {
     return <>
@@ -91,8 +92,5 @@ function BigActionButton(props) {
     </>
 }
 
-function VerticalDivider() {
-    return <div className='border-0 border-end border-2 border-dark-subtle my-3'></div>
-}
 
 export default App;
