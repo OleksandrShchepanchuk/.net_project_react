138bb247aa5403fe0d80bb419a8e17d3b13f103c
Софійка <v.vojcieskiy@gmail.com>
2024-04-07 20:55:53 +0300

    doctor-page

--- hospital/src/components/app/App.js
+++ hospital/src/components/app/App.js
@@ -1,11 +1,24 @@
+import { Doctor } from '../../pages/doctor/doctors';
+import contents from '../../content';
 
 
-function App() {
-  return (
-    <>
 
-    </>
-  );
-}
+export default function App() {
+  return(
+    
+    <div className='App'>
+      {contents.map(content => (
+        <Doctor
+        key = {content.id}
+        image = {content.image}
+        name = {content.name}
+        location = {content.location}
+        specialty = {content.specialty}
+        ratings={content.ratings}
+        time ={content.time}
 
-export default App;
+        />
+      ))}
+    </div>
+  )
+};
