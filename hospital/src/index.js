9a04c8415c00955f9278ce7742925dd89ed2ce0b
Khrystyna Khutka <kkhutka@gmail.com>
2024-04-07 12:13:40 -0500

    Login ready. To add: email check, empty fields should be red

--- hospital/src/index.js
+++ hospital/src/index.js
@@ -3,21 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/app/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/Khrystyna
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-<<<<<<< HEAD
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-=======
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
->>>>>>> origin/Khrystyna
