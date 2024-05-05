15092e9df1ed443d1c72c735a8b9b8a972d65e22
Софійка <v.vojcieskiy@gmail.com>
2024-05-05 20:30:37 +0300

    add Zapyt

--- hospital/src/api/axios.js
+++ hospital/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5075/api", // Adjust the base URL to match your ASP.NET backend
+    baseURL: "http://localhost:5075/api",
 });
 
 axiosInstance.interceptors.request.use((config)=>{
