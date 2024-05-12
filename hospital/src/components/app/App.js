c292c11c373728cbbc4bca13078c3f44b5771751
Софійка <v.vojcieskiy@gmail.com>
2024-05-12 17:55:30 +0300

    update props

--- hospital/src/components/app/App.js
+++ hospital/src/components/app/App.js
@@ -1,32 +1,29 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LoginPage from '../../pages/LoginPage/LoginPage';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+import LoginPage  from '../../pages/LoginPage/LoginPage'
 import ListPage from "../../pages/ListPage/ListPage";
 import Doctor from "../../pages/doctor/doctor";
+import content from '../../content';
 import MainPage from '../../pages/MainPage/MainPage';
-import DoctorsList from "../../components/lists/doctorsList/DoctorsList";
-import DoctorDetails from "../../components/doctor/DoctorDetails"; // Переконайтеся, що шлях до цього компоненту вірний
+import HospitalHeader from '../Header/Header';
 
 function App() {
+  console.log(content);
   return (
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<MainPage />}></Route>
-          <Route path='/login' element={<LoginPage />}></Route>
-          <Route path="/list-page" element={<ListPage />}></Route>
-          <Route path="/doctors" element={<DoctorsList />}></Route> {/* Список лікарів */}
-          <Route path="/doctor/:id" element={<DoctorDetails />}></Route> {/* Деталі конкретного лікаря */}
-          <Route path="/new-doctor-page" element={
-            <Doctor 
-              key = {content.id}
-              image = {content.image}
-              name = {content.name}
-              location = {content.location}
-              specialty = {content.specialty}
-              ratings={content.ratings}
-              time ={content.time}
-            />
-          }></Route>
+          <Route path='/' element={<MainPage/>}></Route>
+          <Route path='/login' element={<LoginPage/>}></Route>
+          <Route path="/list-page" element={<ListPage></ListPage>}></Route>
+          <Route path="/new-doctor-page" element={<Doctor  key = {content.id}
+                    image = {content.image}
+                    name = {content.name}
+                    location = {content.location}
+                    specialty = {content.specialty}
+                    ratings={content.ratings}
+                    time ={content.time}></Doctor>}>
+          </Route>
         </Routes>
       </BrowserRouter>
   );
